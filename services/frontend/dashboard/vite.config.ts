23e56abbe2e41f777ecfee616b55c2af821ad199
Joshua Varghese <joshua.varghese@outlook.com.au>
2026-02-18 00:16:29 +1100

    Dashboard

---
+++ services/frontend/dashboard/vite.config.ts
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true
+      }
+    }
+  }
+})
